29c4bd7b169f12635ce68bc13ef268453e6d5cf4
Chen YEN CHEN <kevin900808@gmail.com>
2024-01-30 22:00:04 +0800

    L3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRouters = require('./routers/workouts')
 
 const app = express()
@@ -19,8 +20,16 @@ app.get('/',(req, res)=>{
 
 app.use('/api/workouts',workoutRouters)
 
-//listen for requests
-app.listen(process.env.PORT, ()=> {
-    console.log('listening on port', process.env.PORT)
-})
+//connect db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen for requests
+        app.listen(process.env.PORT, ()=> {
+            console.log('connect to mongodb and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
 
